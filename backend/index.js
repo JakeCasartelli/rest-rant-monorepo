318e6250ffa2c10011367e02d18c675eb59b102a
Jake Casartelli <casartellijake@yahoo.com>
2023-02-06 21:46:08 -0500

    sign up

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
+//const defineCurrentUser = require('./middleware/defineCurrentUser')
+
 
 // Express Settings
 app.use(cors())
@@ -17,7 +19,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/places', require('./controllers/places'))
 app.use('/users', require('./controllers/users'))
-
+app.use('/authentication', require('./controllers/authentication'))
 // Listen for Connections
 app.listen(process.env.PORT, () => {
     console.log(`Listening on ${process.env.PORT}`)
